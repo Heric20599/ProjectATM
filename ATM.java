4ca62d2d236487203d24d3d0a590bf2aca3c85d5
Heric20599 <141494309+Heric20599@users.noreply.github.com>
2023-09-13 00:09:49 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,17 @@
+package PROJECTATM;
+
+import java.io.IOException;
+
+public class ATM {
+
+    public static void main(String[] args) throws IOException {
+        OptionMenu optionMenu = new OptionMenu();
+        introduction();
+        optionMenu.mainMenu();
+    }
+
+    public static void introduction() {
+        System.out.println("Welcome to the ATM Project!");
+    }
+}
+
